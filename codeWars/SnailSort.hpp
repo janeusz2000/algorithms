2b6c6454e1a841afee27a79f0df99ed0f854e851
Janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2021-04-16 20:02:33 +0200

    snail sort works!

--- codeWars/SnailSort.hpp
+++ codeWars/SnailSort.hpp
@@ -4,9 +4,10 @@
 #define SNAIL_SORT_H
 
 #include <cmath>
+#include <iostream>
 #include <vector>
 
-std::vector<int> createRange(int size) {
+std::vector<int> createRange(size_t size) {
   std::vector<int> outputSequence;
   outputSequence.reserve(size);
   for (size_t index = 0; index < size; ++index) {
@@ -15,11 +16,120 @@ std::vector<int> createRange(int size) {
   return outputSequence;
 }
 
-std::vector<std::pair<int, int>> createSequence(int size) {
-  const std::vector<int> range = createRange(size);
-  return {};
+void printVec(const std::vector<int> &v) {
+  std::cout << "[";
+  for (const int number : v) {
+    std::cout << number << ", ";
+  }
+  std::cout << std::endl;
 }
 
-std::vector<int> snail(std::vector<std::vector<int>> &snail_map) { return {}; }
+std::vector<int> createXSequence(size_t size) {
+  const std::vector<int> range = createRange(size);
+
+  std::vector<int> xSequence;
+  xSequence.reserve(std::pow(size, 2));
+
+  int currentSize = size;
+  auto xStart = range.cbegin();
+  auto xStop = range.cend();
+
+  auto xRStart = range.crbegin();
+  auto xRStop = range.crend();
+
+  while (true) {
+
+    for (auto it = xStart; it != xStop; ++it) {
+      xSequence.push_back(*it);
+    }
+
+    if (xSequence.size() >= std::pow(size, 2)) {
+      break;
+    }
+
+    for (int temp = 0; temp < currentSize - 2; ++temp) {
+      xSequence.push_back(*xRStart);
+    }
+
+    for (auto it = xRStart; it != xRStop; ++it) {
+      xSequence.push_back(*it);
+    }
+
+    for (int temp = 0; temp < currentSize - 2; ++temp) {
+      xSequence.push_back(*xStart);
+    }
+
+    currentSize -= 2;
+    --xStop;
+    --xRStop;
+    ++xStart;
+    ++xRStart;
+  }
+  return xSequence;
+}
+
+std::vector<int> createYSequence(size_t size) {
+  std::vector<int> ySequence;
+
+  ySequence.reserve(std::pow(size, 2));
+
+  int yStart = 0;
+  int yStop = size;
+
+  int yRStart = size;
+  int yRStop = 0;
+
+  for (int currentElements = 0; true;) {
+
+    for (int index = yStart; index < yStop; ++index) {
+      ySequence.push_back(yStart);
+      currentElements++;
+    }
+
+    for (int index = yStart + 1; index < yStop; ++index) {
+      ySequence.push_back(index);
+      currentElements++;
+    }
+
+    for (int index = yRStart - 1; index > yRStop + 1; --index) {
+      ySequence.push_back(yStop - 1);
+      currentElements++;
+    }
+
+    if (currentElements >= std::pow(size, 2)) {
+      break;
+    }
+
+    for (int index = yRStart - 1; index > yRStop; --index) {
+      ySequence.push_back(index);
+      currentElements++;
+    }
+
+    ++yStart;
+    ++yRStop;
+    --yRStart;
+    --yStop;
+  }
+  return ySequence;
+}
+
+std::vector<int> snail(std::vector<std::vector<int>> &snail_map) {
+  std::vector<int> output;
+  if (snail_map[0].size() != 0) {
+    output.reserve(std::pow(snail_map.size(), 2));
+
+    std::vector<int> xSequence = createXSequence(snail_map[0].size());
+    std::vector<int> ySequence = createYSequence(snail_map[0].size());
+    printVec(xSequence);
+    printVec(ySequence);
+
+    for (size_t index = 0; index < xSequence.size(); ++index) {
+      int y = xSequence[index];
+      int x = ySequence[index];
+      output.push_back(snail_map[x][y]);
+    }
+  }
+  return output;
+}
 
 #endif
--- tests/snailSort_test.cpp
+++ tests/snailSort_test.cpp
@@ -5,13 +5,63 @@
 #include <vector>
 
 using ::testing::ElementsAreArray;
-using CoordinateSequence = std::vector<std::pair<int, int>>;
+using Sequence = std::vector<int>;
+TEST(SnailSort, XSequenceTest) {
+  const Sequence obtainedXSequence0 = createXSequence(0);
+  const Sequence expectedXSequence0 = {};
+  ASSERT_THAT(obtainedXSequence0, ElementsAreArray(expectedXSequence0));
 
-TEST(SnailSort, sequenceTest) {
-  CoordinateSequence obtainedSequence = createSequence(3, 3);
-  CoordinateSequence expectedSequence = {{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2},
-                                         {1, 2}, {0, 2}, {0, 1}, {1, 1}};
-  ASSERT_THAT(obtainedSequence, ElementsAreArray(expectedSequence));
+  const Sequence obtainedXSequence1 = createXSequence(1);
+  const Sequence expectedXSequence1 = {0};
+  ASSERT_THAT(obtainedXSequence1, ElementsAreArray(expectedXSequence1));
+
+  const Sequence obtainedXSequence2 = createXSequence(2);
+  const Sequence expectedXSequence2 = {0, 1, 1, 0};
+  ASSERT_THAT(obtainedXSequence2, ElementsAreArray(expectedXSequence2));
+
+  const Sequence obtainedXSequence3 = createXSequence(3);
+  const Sequence expectedXSequence3 = {0, 1, 2, 2, 2, 1, 0, 0, 1};
+  ASSERT_THAT(obtainedXSequence3, ElementsAreArray(expectedXSequence3));
+  const Sequence obtainedXSequence4 = createXSequence(4);
+  const Sequence expectedXSequence4 = {0, 1, 2, 3, 3, 3, 3, 2,
+                                       1, 0, 0, 0, 1, 2, 2, 1};
+  ASSERT_THAT(obtainedXSequence4, ElementsAreArray(expectedXSequence4));
+
+  const Sequence obtainedXSequence5 = createXSequence(5);
+  const Sequence expectedXSequence5 = {0, 1, 2, 3, 4, 4, 4, 4, 4, 3, 2, 1, 0,
+                                       0, 0, 0, 1, 2, 3, 3, 3, 2, 1, 1, 2};
+  ASSERT_THAT(obtainedXSequence5, ElementsAreArray(expectedXSequence5));
+}
+
+TEST(SnailSort, YSequenceTest) {
+
+  const Sequence obtainedYSequence0 = createYSequence(0);
+  const Sequence expectedYSequence0 = {};
+  ASSERT_THAT(obtainedYSequence0, ElementsAreArray(expectedYSequence0));
+
+  const Sequence obtainedYSequence1 = createYSequence(1);
+  const Sequence expectedYSequence1 = {0};
+  ASSERT_THAT(obtainedYSequence1, ElementsAreArray(expectedYSequence1));
+
+  const Sequence obtainedYSequence2 = createYSequence(2);
+  const Sequence expectedYSequence2 = {0, 0, 1, 1};
+  ASSERT_THAT(obtainedYSequence2, ElementsAreArray(expectedYSequence2));
+
+  const Sequence obtainedYSequence3 = createYSequence(3);
+  const Sequence expectedYSequence3 = {0, 0, 0, 1, 2, 2, 2, 1, 1};
+  ASSERT_THAT(obtainedYSequence3, ElementsAreArray(expectedYSequence3))
+      << "Expected: { 0, 0, 0, 1, 2, 2, 2, 1, 1}";
+
+  const Sequence obtainedYSequence4 = createYSequence(4);
+  const Sequence expectedYSequence4 = {0, 0, 0, 0, 1, 2, 3, 3,
+                                       3, 3, 2, 1, 1, 1, 2, 2};
+  ASSERT_THAT(obtainedYSequence4, ElementsAreArray(expectedYSequence4))
+      << "Expected: [ 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 2, 1, 1, 1, 2, 2";
+
+  const Sequence obtainedYSequence5 = createYSequence(5);
+  const Sequence expectedYSequence5 = {0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4,
+                                       3, 2, 1, 1, 1, 1, 2, 3, 3, 3, 2, 2};
+  ASSERT_THAT(obtainedYSequence5, ElementsAreArray(expectedYSequence5));
 }
 
 TEST(SnailSort, CataTest) {
@@ -21,6 +71,7 @@ TEST(SnailSort, CataTest) {
 
   v = {{}};
   expected = {};
+  std::cout << "first size: " << v.size() << std::endl;
   ASSERT_THAT(snail(v), ElementsAreArray(expected));
 
   v = {{1}};
