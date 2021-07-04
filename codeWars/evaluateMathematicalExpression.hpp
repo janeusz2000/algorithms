f740788d6e46d18749279d1b710976fe0cc409df
Janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2021-07-04 22:24:38 +0200

    rvaluate mathematical expression works :D

--- codeWars/evaluateMathematicalExpression.hpp
+++ codeWars/evaluateMathematicalExpression.hpp
@@ -15,7 +15,8 @@ enum class TokenType {
   MINUS,
   MULTIPLIES,
   DIVIDES,
-  UNARY_OPERATOR
+  UNARY_OPERATOR,
+  OPEN_PARENTHESIS,
 };
 
 class TokenFactory;
@@ -44,10 +45,15 @@ TokenType Token::getType() const { return tokenType_; }
 double Token::value() const { return value_; }
 
 const std::unordered_map<TokenType, std::function<double(double, double)>>
-    operationFunctions{{TokenType::PLUS, std::plus<>()},
-                       {TokenType::MINUS, std::minus<>()},
-                       {TokenType::MULTIPLIES, std::multiplies<>()},
-                       {TokenType::DIVIDES, std::divides<>()}};
+    doubleArgumentOperations{{TokenType::PLUS, std::plus<>()},
+                             {TokenType::MINUS, std::minus<>()},
+                             {TokenType::MULTIPLIES, std::multiplies<>()},
+                             {TokenType::DIVIDES, std::divides<>()}};
+
+const std::unordered_map<TokenType, std::function<double(double)>>
+    singleArgumentOperations{
+        {TokenType::UNARY_OPERATOR, [](double number) { return -number; }}};
+
 class TokenFactory {
 public:
   explicit TokenFactory()
@@ -55,11 +61,10 @@ public:
                     {'-', TokenType::MINUS},
                     {'/', TokenType::DIVIDES},
                     {'*', TokenType::MULTIPLIES}},
-        precedences_{{TokenType::NUMBER, 0},
-                     {TokenType::PLUS, 1},
-                     {TokenType::MINUS, 1},
-                     {TokenType::DIVIDES, 2},
-                     {TokenType::MULTIPLIES, 2}} {};
+        precedences_{
+            {TokenType::NUMBER, 0},     {TokenType::PLUS, 1},
+            {TokenType::MINUS, 1},      {TokenType::DIVIDES, 2},
+            {TokenType::MULTIPLIES, 2}, {TokenType::UNARY_OPERATOR, 3}} {};
 
   Token createOperationToken(char operation) const {
     return Token(operations_.at(operation), 0, *this);
@@ -67,6 +72,10 @@ public:
 
   Token createNumberToken(double value) const {
     return Token(TokenType::NUMBER, value, *this);
+  }
+
+  Token createToken(TokenType tokenType, double value) {
+    return Token(tokenType, value, *this);
   }
 
   int getPrecedence(TokenType tokenType) const {
@@ -88,34 +97,45 @@ std::vector<Token> parseExpression(std::string_view expression,
                                    TokenFactory *tokenFactory) {
   std::vector<Token> output;
   std::vector<Token> operatorStack;
-
+  bool wasOperator = false;
   for (size_t index = 0; index < expression.length(); index++) {
     if (tokenFactory->getOperations().find(expression[index]) !=
         tokenFactory->getOperations().end()) {
+
       Token operationToken =
-          tokenFactory->createOperationToken(expression[index]);
+          (output.empty() || wasOperator)
+              ? tokenFactory->createToken(TokenType::UNARY_OPERATOR, -1)
+              : tokenFactory->createOperationToken(expression[index]);
+      wasOperator = true;
 
-      if (operatorStack.empty() ||
-          (!operatorStack.empty() &&
-           operatorStack[operatorStack.size() - 1].getPrecedence() <=
-               operationToken.getPrecedence())) {
-        operatorStack.push_back(operationToken);
-      } else {
-        output.insert(output.end(), operatorStack.rbegin(),
-                      operatorStack.rend());
-
-        operatorStack.clear();
-        operatorStack.push_back(operationToken);
+      while (operatorStack.size() > 0 &&
+             operatorStack.back().getType() != TokenType::OPEN_PARENTHESIS &&
+             operatorStack.back().getPrecedence() >=
+                 operationToken.getPrecedence()) {
+        output.push_back(operatorStack.back());
+        operatorStack.pop_back();
       }
+
+      operatorStack.push_back(operationToken);
 
     } else if (isdigit(expression[index])) {
       char *endPoint;
       double value = std::strtod(expression.data() + index, &endPoint);
       index += endPoint - (expression.data() + index) - 1;
       output.push_back(tokenFactory->createNumberToken(value));
+      wasOperator = false;
+    } else if (expression[index] == '(') {
+      operatorStack.push_back(
+          tokenFactory->createToken(TokenType::OPEN_PARENTHESIS, 0));
+    } else if (expression[index] == ')') {
+      while (operatorStack.back().getType() != TokenType::OPEN_PARENTHESIS) {
+        output.push_back(operatorStack.back());
+        operatorStack.pop_back();
+      }
+
+      operatorStack.pop_back();
     }
   }
-
   if (!operatorStack.empty()) {
     output.insert(output.end(), operatorStack.rbegin(), operatorStack.rend());
   }
@@ -123,30 +143,46 @@ std::vector<Token> parseExpression(std::string_view expression,
   return output;
 }
 
-double performOperation(Token operation, Token number1, Token number2) {
-  return operationFunctions.at(operation.getType())(number1.value(),
-                                                    number2.value());
+double performDoubleArgumentOperation(Token operation, Token number1,
+                                      Token number2) {
+  return doubleArgumentOperations.at(operation.getType())(number1.value(),
+                                                          number2.value());
+}
+double performSingleArgumentOperation(Token operation, Token number) {
+  return singleArgumentOperations.at(operation.getType())(number.value());
 }
 
-double solve(std::string_view expression) {
+double calc(std::string_view expression) {
   TokenFactory tokenFactory;
   std::vector<Token> parsedExpression =
       parseExpression(expression, &tokenFactory);
+
   while (parsedExpression.size() != 1) {
     auto operationToken = std::find_if(
         parsedExpression.begin(), parsedExpression.end(), [&](Token token) {
-          return operationFunctions.find(token.getType()) !=
-                 operationFunctions.end();
+          return doubleArgumentOperations.find(token.getType()) !=
+                     doubleArgumentOperations.end() ||
+                 singleArgumentOperations.find(token.getType()) !=
+                     singleArgumentOperations.end();
         });
 
     Token operation = *operationToken;
-    Token number2 = *(operationToken - 1);
-    Token number1 = *(operationToken - 2);
+    if (doubleArgumentOperations.find(operation.getType()) !=
+        doubleArgumentOperations.end()) {
+      Token number2 = *(operationToken - 1);
+      Token number1 = *(operationToken - 2);
 
-    double result = performOperation(operation, number1, number2);
-    parsedExpression.erase(operationToken);
-    parsedExpression.erase(operationToken - 1);
-    *(operationToken - 2) = tokenFactory.createNumberToken(result);
+      double result =
+          performDoubleArgumentOperation(operation, number1, number2);
+      parsedExpression.erase(operationToken);
+      parsedExpression.erase(operationToken - 1);
+      *(operationToken - 2) = tokenFactory.createNumberToken(result);
+    } else {
+      Token number = *(operationToken - 1);
+      double result = performSingleArgumentOperation(operation, number);
+      parsedExpression.erase(operationToken);
+      *(operationToken - 1) = tokenFactory.createNumberToken(result);
+    }
   }
   return parsedExpression[0].value();
 }
--- tests/calculateExpression_test.cpp
+++ tests/calculateExpression_test.cpp
@@ -3,8 +3,12 @@
 #include "gtest/gtest.h"
 
 TEST(CodeWarsTest, basicTest) {
-  ASSERT_FLOAT_EQ(solve("12 + 4 * 2 / 4 + 3 * 8 / 3"), 22);
-
-  ASSERT_FLOAT_EQ(solve("12.001 + 4 * 2 / 4 + 3 * 8 / 3"), 22.001);
-  ASSERT_FLOAT_EQ(solve("1e-2 * 100 + 40 * 2 + 20"), 101);
+  ASSERT_FLOAT_EQ(calc("12 + 4 * 2 / 4 + 3 * 8 / 3"), 22);
+  ASSERT_FLOAT_EQ(calc("-1 * 8 + 3 * -5 / 3"), -13);
+  ASSERT_FLOAT_EQ(calc("12.001 + 4 * 2 / 4 + 3 * 8 / 3"), 22.001);
+  ASSERT_FLOAT_EQ(calc("1e-2*100+40*2+20"), 101);
+  ASSERT_FLOAT_EQ(calc("2 * (1 + 3 * 5) / 16 + 5 * 4 * (2 + 1)"), 62);
+  ASSERT_FLOAT_EQ(calc("(((((-1)))))"), -1);
 }
+
+// -1 *
